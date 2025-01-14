1130b3603fede475f104a9d58b1fd0bde3be8c13
Asher Lien <birdym.tw@gmail.com>
2025-01-14 14:26:59 +0800

    完成切版

--- src/pages/dashboard/profile/emailVerify.vue
+++ src/pages/dashboard/profile/emailVerify.vue
@@ -1,5 +1,37 @@
 <script setup lang='ts'>
-import { NCard } from 'naive-ui'
+import { NCard, NButton, NSpace, NInput, NInputGroup, NText } from 'naive-ui'
+import { storeToRefs } from 'pinia'
+
+import EmailVerifyComponent from '@/components/app/dashboard/profile/EmailVerifyComponent.vue'
+import { useUserStore } from '@/stores'
+
+const userStore = useUserStore()
+const { getUser } = storeToRefs(userStore)
+
+enum Page {
+	emailVerify = 'emailVerify',
+	emailAlreadyVerify = 'emailAlreadyVerify'
+}
+
+const state = ref({
+	data: {
+		verifyCode: ''
+	},
+	page: {
+		current: Page.emailVerify
+	}
+})
+
+const router = useRouter()
+
+onMounted(() => {
+	console.log(getUser.value)
+
+	if (getUser.value?.email) {
+		state.value.page.current = Page.emailAlreadyVerify
+	}
+
+})
 </script>
 
 <template>
@@ -8,17 +40,52 @@ import { NCard } from 'naive-ui'
       vertical
       justify="center"
     >
+      <!--
+        驗證Email
+      -->
       <n-card
+        v-if="state.page.current === Page.emailVerify"
         title="驗證Email"
         class="min-w-[290px]"
       >
-        Card Content
-        <template #footer>
-          #footer
-        </template>
+        <n-text>
+          {{ `請前往您的Email信箱${getUser?.email}，輸入驗證碼以完成驗證。` }}
+        </n-text>
+        <n-input-group class="mt-2">
+          <n-input
+            v-model:value="state.data.verifyCode"
+            placeholder="請輸入驗證碼"
+          />
+          <EmailVerifyComponent />
+        </n-input-group>
         <template #action>
-          #action
+          <n-button
+            block
+            type="primary"
+          >
+            驗證
+          </n-button>
         </template>
+      </n-card>
+      <!--
+        已驗證Email
+      -->
+      <n-card
+        v-else
+        title="Email已驗證"
+        class="min-w-[290px]"
+      >
+        <n-text>
+          {{ `${getUser?.email}已驗證成功。` }}
+        </n-text>
+        <n-button
+          class="mt-2"
+          block
+          type="primary"
+          @click="router.back()"
+        >
+          返回
+        </n-button>
       </n-card>
     </n-space>
   </div>
