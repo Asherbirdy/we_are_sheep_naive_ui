fb2bb88bd6699909de3e5a4a0dcdc60e7d0b4877
Asher Lien <birdym.tw@gmail.com>
2025-01-15 17:30:54 +0800

    更新

--- src/components/common/MenubarComponent.vue
+++ src/components/common/MenubarComponent.vue
@@ -1,98 +1,23 @@
 <script setup lang='ts'>
-import {
-	BookOutline as BookIcon,
-	PersonOutline as PersonIcon,
-	WineOutline as WineIcon
-} from '@vicons/ionicons5'
+
 import { Menu2 as MenuIcon } from '@vicons/tabler'
 import { NIcon, NDrawer, NDrawerContent, NMenu, NDropdown } from 'naive-ui'
-import type { DropdownProps, MenuOption } from 'naive-ui'
+import type { DropdownProps } from 'naive-ui'
+import { storeToRefs } from 'pinia'
 
 import ThemeSwichComponent from './ThemeSwichComponent.vue'
-import { renderIcon } from '@/utils'
-
+import { useMenuStore } from '@/stores'
+const menuStore = useMenuStore()
+const { menu } = storeToRefs(menuStore)
 const state = ref({
-	dropdown: {
-		show: false
+	status: {
+		dropdown: false,
+		drawer: false
 	}
 })
 
 const active = ref(false)
 const activeKey = ref('')
-const menuOptions: MenuOption[] = [
-	{
-		label: 'Hear the Wind Sing',
-		key: 'hear-the-wind-sing',
-		icon: renderIcon(BookIcon)
-	},
-	{
-		label: 'Pinball 1973',
-		key: 'pinball-1973',
-		icon: renderIcon(BookIcon),
-		disabled: true,
-		children: [
-			{
-				label: 'Rat',
-				key: 'rat'
-			}
-		]
-	},
-	{
-		label: 'A Wild Sheep Chase',
-		key: 'a-wild-sheep-chase',
-		disabled: true,
-		icon: renderIcon(BookIcon)
-	},
-	{
-		label: 'Dance Dance Dance',
-		key: 'Dance Dance Dance',
-		icon: renderIcon(BookIcon),
-		children: [
-			{
-				type: 'group',
-				label: 'People',
-				key: 'people',
-				children: [
-					{
-						label: 'Narrator',
-						key: 'narrator',
-						icon: renderIcon(PersonIcon)
-					},
-					{
-						label: 'Sheep Man',
-						key: 'sheep-man',
-						icon: renderIcon(PersonIcon)
-					}
-				]
-			},
-			{
-				label: 'Beverage',
-				key: 'beverage',
-				icon: renderIcon(WineIcon),
-				children: [
-					{
-						label: 'Whisky',
-						key: 'whisky'
-					}
-				]
-			},
-			{
-				label: 'Food',
-				key: 'food',
-				children: [
-					{
-						label: 'Sandwich',
-						key: 'sandwich'
-					}
-				]
-			},
-			{
-				label: 'The past increases. The future recedes.',
-				key: 'the-past-increases-the-future-recedes'
-			}
-		]
-	}
-]
 
 const dropdownOptions = [
 	{
@@ -135,7 +60,7 @@ const dropdownLabel: DropdownProps['renderLabel'] = (dropdownOptions) => {
 			網頁版 menu dropdown
 		-->
     <n-dropdown
-      :show="state.dropdown.show"
+      :show="state.status.dropdown"
       :options="dropdownOptions"
       placement="bottom-start"
       :render-label="dropdownLabel"
@@ -145,7 +70,7 @@ const dropdownLabel: DropdownProps['renderLabel'] = (dropdownOptions) => {
         :size="30"
         color="gray"
         cursor="pointer"
-        @click="state.dropdown.show = !state.dropdown.show"
+        @click="state.status.dropdown = !state.status.dropdown"
       >
         <MenuIcon />
       </n-icon>
@@ -154,13 +79,13 @@ const dropdownLabel: DropdownProps['renderLabel'] = (dropdownOptions) => {
       v-model:show="active"
       placement="right"
     >
-      <n-drawer-content title="Stoner">
+      <n-drawer-content title="Menu">
         <ThemeSwichComponent />
         <n-menu
           v-model:value="activeKey"
           :root-indent="36"
           :indent="12"
-          :options="menuOptions"
+          :options="menu"
         />
       </n-drawer-content>
     </n-drawer>
