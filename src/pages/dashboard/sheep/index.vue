b04d1fce3d5637ae457175fda50ae9302deae270
Asher Lien <birdym.tw@gmail.com>
2025-01-16 17:16:54 +0800

    wip

--- src/pages/dashboard/sheep/index.vue
+++ src/pages/dashboard/sheep/index.vue
@@ -1,7 +1,7 @@
 <script setup lang='ts'>
 import { useQuery } from '@tanstack/vue-query'
 import type { DataTableColumns } from 'naive-ui'
-import { NButton, NTag, useMessage, NDataTable, NTabPane, NTabs } from 'naive-ui'
+import { NButton, NTag, useMessage, NDataTable, NTabPane, NTabs, NSpace } from 'naive-ui'
 
 import { QueryKeyEnum } from '@/enums'
 import { useSheepApi } from '@/hook'
@@ -11,6 +11,20 @@ interface RowData {
 	name: string
 	tags: string[]
 }
+
+enum Page {
+	home = 'home',
+	details = 'details',
+}
+
+const state = ref({
+	data: {
+		details: null
+	},
+	page: {
+		current: Page.home
+	}
+})
 
 // 取得牧養名單
 const { data: handleSheepList } = useQuery({
@@ -73,32 +87,43 @@ const createColumns = (): DataTableColumns<RowData> => {
 
 <template>
   <div>
-    <n-tabs
-      type="segment"
-      animated
+    <n-space
+      v-if="state.page.current === Page.home"
+      vertical
     >
-      <n-tab-pane
-        name="focus"
-        tab="重點牧養"
+      <n-tabs
+        type="segment"
+        animated
       >
-        <n-data-table
-          :bordered="false"
-          :single-line="false"
-          :columns="createColumns()"
-          :data="handleSheepList?.list.focusPersonList"
-        />
-      </n-tab-pane>
-      <n-tab-pane
-        name="non-focus"
-        tab="其他名單"
-      >
-        <n-data-table
-          :bordered="false"
-          :single-line="false"
-          :columns="createColumns()"
-          :data="handleSheepList?.list.nonFocusPersonList"
-        />
-      </n-tab-pane>
-    </n-tabs>
+        <n-tab-pane
+          name="focus"
+          tab="重點牧養"
+        >
+          <n-data-table
+            :bordered="false"
+            :single-line="false"
+            :columns="createColumns()"
+            :data="handleSheepList?.list.focusPersonList"
+          />
+        </n-tab-pane>
+        <n-tab-pane
+          name="non-focus"
+          tab="其他名單"
+        >
+          <n-data-table
+            :bordered="false"
+            :single-line="false"
+            :columns="createColumns()"
+            :data="handleSheepList?.list.nonFocusPersonList"
+          />
+        </n-tab-pane>
+      </n-tabs>
+    </n-space>
+    <n-space
+      v-else
+      vertical
+    >
+      Edit
+    </n-space>
   </div>
 </template>
