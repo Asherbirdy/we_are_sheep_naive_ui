4c65ca4288c2a0744f427027f22ea5f82a0ef462
Asher Lien <birdym.tw@gmail.com>
2025-01-25 12:15:48 +0800

    e? v

--- src/pages/dashboard/sheep/index.vue
+++ src/pages/dashboard/sheep/index.vue
@@ -88,7 +88,6 @@ const createColumns = (): DataTableColumns<PersonList> => {
 								details.name = row.name
 								details.ageRange = row.ageRange
 								details.personStatus = row.personStatus
-
 								details.weekInviteTag = row.weekInviteTag
 								details.identity = row.identity
 								details.focusPerson = row.focusPerson
