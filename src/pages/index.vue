ba6e8fbbd29144f95bcf0f1a844b1339961da0e0
Asher Lien <birdym.tw@gmail.com>
2025-02-03 17:44:44 +0800

    done

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,6 +9,7 @@ import { NSpace, NImage, NCard, NTabPane, NTabs } from 'naive-ui'
 import { useI18n } from 'vue-i18n'
 
 import WelcomeImage from '@/assets/image/login/welcome.png'
+import { DashboardRoutes } from '@/enums'
 import { useAuthApi } from '@/hook'
 const router = useRouter()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, unused-imports/no-unused-vars
@@ -17,7 +18,7 @@ const { t } = useI18n()
 const init = async () => {
 	const res = await useAuthApi.checkValidToken.api()
 	if (res.msg === 'Token is valid') {
-		router.push('/dashboard')
+		router.push(DashboardRoutes.districtSheep)
 	}
 }
 
