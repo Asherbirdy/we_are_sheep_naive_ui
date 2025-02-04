46ad5ea7c62557b8ead4be27e08edaf6f0ef270d
Asher Lien <birdym.tw@gmail.com>
2025-02-04 17:32:56 +0800

    done

--- src/types/api/SheepApiType.ts
+++ src/types/api/SheepApiType.ts
@@ -44,3 +44,33 @@ export interface EditSheepPayload {
     note: string
   }
 }
+
+// * Get User District Sheep
+export interface GetUserDistrictSheepResponse {
+  msg: string
+  data: {
+    male: DistrictSheep[]
+    female: DistrictSheep[]
+    noFocus: DistrictSheep[]
+  }
+}
+
+export interface DistrictSheep {
+  _id: string
+  name: string
+  ageRange: string
+  tags?: string[]
+  focusPerson: string
+  userId: {
+    _id: string
+    name: string
+  }
+  personStatus: string
+  note: string
+  createdAt: string
+  updatedAt: string
+  __v: number
+  district: string
+  weekInviteTag: string[]
+  identity: string
+}
