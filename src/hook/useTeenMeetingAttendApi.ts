f74854ecc59e1e3b0326bfe92f5888fa5d49e9e6
Asher Lien <birdym.tw@gmail.com>
2025-02-17 15:22:23 +0800

    aip

--- src/hook/useTeenMeetingAttendApi.ts
+++ src/hook/useTeenMeetingAttendApi.ts
@@ -1,10 +1,41 @@
 import useRequest from './http'
+export interface TeenMeetingAttendApiGetResponse {
+  message: string
+  data: Data
+}
+
+export interface Data {
+  ageRange: {
+    nonSelected: Daum
+    teenager: Daum
+    college: Daum
+    youth: Daum
+    child1: Daum
+    child2: Daum
+    child3: Daum
+  }
+}
+
+export interface Daum {
+  _id: string
+  district: string
+  name: string
+  ageRange: string
+  gender: string
+  participation: string
+  meetingStatus: string
+  sheepherd: string
+  inviteList: string
+  createdAt: string
+  updatedAt: string
+  __v: number
+}
 
 export const useTeenMeetingAttendApi = {
   get: async (payload: {
     password: string
   }) => await useRequest.post({
-    url: '/teenMeetingAttend/get',
-    data: payload.password
+    url: '/teemMeetingAttend/get',
+    data: payload
   })
 }
--- src/pages/teenMeetingAttend.vue
+++ src/pages/teenMeetingAttend.vue
@@ -1,3 +1,76 @@
+<route lang="yaml">
+  name: 'teenMeetingAttend'
+  meta:
+    layout: login
+</route>
+<script setup lang='ts'>
+import { useMutation } from '@tanstack/vue-query'
+import { NCard, NInput, NButton } from 'naive-ui'
+
+import { useTeenMeetingAttendApi } from '@/hook'
+
+enum Page {
+	password = 'password',
+	dataPage = 'dataPage',
+}
+
+const message = useMessage()
+
+const state = ref(
+	{
+		api: {
+			getTeenMeetingAttend: null
+		},
+		data: {
+			password: ''
+		},
+		current: {
+			page: Page.password
+		},
+		isDisabled: true
+	}
+)
+
+const { mutate: handleGetTeenMeetingAttend, isPending } = useMutation({
+	mutationFn: () => useTeenMeetingAttendApi.get({ password: state.value.data.password }),
+	onSuccess: (data) => {
+
+		if (data?.response?.status === 401) {
+			message.error('密碼錯誤')
+			return
+		}
+
+		state.value.current.page = Page.dataPage
+		state.value.api.getTeenMeetingAttend = data
+		console.log('state.value.api.getTeenMeetingAttend', state.value.api.getTeenMeetingAttend)
+	}
+})
+
+watch(state.value.data, (val) => {
+	if (val.password.length >= 3) {
+		state.value.isDisabled = false
+	}
+})
+
+</script>
 <template>
-  <div>ss</div>
+  <div>
+    <n-card
+      v-if="state.current.page === Page.password"
+      title="密碼"
+    >
+      <n-input v-model:value="state.data.password" />
+      <n-button
+        :disabled="state.isDisabled"
+        type="primary"
+        :loading="isPending"
+        @click="handleGetTeenMeetingAttend()"
+      >
+        確認
+      </n-button>
+    </n-card>
+    <div v-if="state.current.page === Page.dataPage">
+      {{ state.api.getTeenMeetingAttend }}
+    </div>
+  </div>
 </template>
