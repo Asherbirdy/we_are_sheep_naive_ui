0d524a684029314b9e77674212799875d4a6adb4
Asher Lien <birdym.tw@gmail.com>
2025-03-07 16:38:27 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,26 @@ export default defineConfig(({ mode }) => {
     base: env.VITE_BASE || '/',
     plugins: [
       vue(),
-      VitePWA(),
+      VitePWA({
+        manifest: {
+          name: 'Name of your app',
+          short_name: 'Short name of your app',
+          description: 'Description of your app',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: '/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: '/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      }),
       AutoImport({
         imports: [
           'vue',
