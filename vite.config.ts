bc8f742b2d12c5f6ef3322b842fa18d49c25b2b6
Asher Lien <birdym.tw@gmail.com>
2025-03-24 09:28:06 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import Layouts from 'vite-plugin-vue-layouts'
 // https://vitejs.dev/config/
 
+//
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   return {
